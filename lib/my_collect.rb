50cd75584a3170188cd7d4fbd8271dc885b164c1
David Strauch <dmaster18@gmail.com>
2019-07-27 17:51:07 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -5,7 +5,7 @@ def my_collect(array)
     collection << yield(array[i])
     i+=1
   end
-  puts collection
+  collection
 end
 
 array = ["Bob Jones", "Joe Jackson", "John Williams", "Homer Simpson"]
